30c2cf7c30e2555828082819a95b1144793f3c1d
GitHub Action <action@github.com>
2025-09-07 13:38:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/suj1e/icommit/MyBundle.kt
+++ src/main/kotlin/com/github/suj1e/icommit/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.suj1e.icommit
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/suj1e/icommit/services/MyProjectService.kt
+++ src/main/kotlin/com/github/suj1e/icommit/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.suj1e.icommit.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.suj1e.icommit.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
