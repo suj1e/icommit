cf5f7e3f4919896c0133ce7a5f7e397730035433
sujie <13156660820@163.com>
2025-09-12 00:45:52 +0800

    feat: add model

--- src/main/java/org/flooc/plugin/icommit/constant/VolcConstant.java
+++ src/main/java/org/flooc/plugin/icommit/constant/VolcConstant.java
@@ -11,7 +11,8 @@ public final class VolcConstant {
   public static final String DEFAULT_URL = "https://ark.cn-beijing.volces.com/api/v3";
 
   public static final String DEFAULT_MODEL = "doubao-seed-1-6-flash-250828";
-
+  public static final String DOUBAO_SEED_1_6_THINKING = "doubao-seed-1-6-thinking-250715";
   public static final String DOUBAO_SEED_1_6_250615 = "doubao-seed-1-6-250615";
+
 
 }
